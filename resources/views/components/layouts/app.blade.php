230f6bda9be4398b09b512525c336201ff1b68dc
Error404393 <kenasyah12@gmail.com>
2026-05-11 16:59:22 +0700

    disable vite temporary

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $title ?? 'Nusantara' }}</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=DM+Sans:ital,opsz,wght@0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;1,9..40,400&display=swap" rel="stylesheet">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
