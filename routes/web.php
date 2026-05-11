2a8935390c30f93ba913847cf7a713155e9fe437
Error404393 <kenasyah12@gmail.com>
2026-05-11 17:18:50 +0700

    test plain route

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\AnggotaController;
 use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
-    return 'WEB BERHASIL';
+    return 'Laravel hidup';
 });
 
 // Route::get('/', function () {
